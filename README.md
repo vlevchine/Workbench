2b50b177c83bd41dbcaaa5e37f0fda16c1d22eff
vlevchine <valevchine@yahoo.com>
2016-08-09 11:02:27 -0600

    linting chnages

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+Travis status
 [![Build Status](https://travis-ci.org/vlevchine/Workbench.svg?branch=master)](https://travis-ci.org/vlevchine/Workbench)
--- main.js
+++ main.js
@@ -12,8 +12,8 @@ app.use(bodyParser.json());
 
 app.use(Express.static('./app'));
 app.get('/messages',(req,res)=>{
-	res.status(200).json(require('./messages.json'))
-})
+	res.status(200).json(require('./messages.json'));
+});
 app.listen(port,()=>{
 	//console.log(`Running webhook listener...`);
 	console.log(`App listening on port ${port}`);
